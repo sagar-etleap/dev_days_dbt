6bfb6fbe5f16b7b23d6f7be3ca8250ff0480f3f9
Sagar <sagar@etleap.com>
2026-01-06 04:00:29 -0500

    use basic query (#2)

--- models/customer_summary.sql
+++ models/customer_summary.sql
@@ -1,5 +1,1 @@
-SELECT 
-	id AS salesforce_id, 
-    zenith_account_id__c AS account_id, 
-    name, account_number, annual_revenue 
-FROM {{ source('PUBLIC', 'SALESFORCE_ACCOUNT') }}
+SELECT 1 as ok 
